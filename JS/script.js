ebb2b418a65457b5c4cd22d59e9fb446eca76701
ClaudioLeone <claleone92@gmail.com>
2023-03-14 19:03:31 +0100

    Completed 'next button' and 'previous button' logic [MILESTONE III] + added background color and site title

--- JS/script.js
+++ JS/script.js
@@ -16,3 +16,41 @@ for (let i = 0; i < imagesArray.length; i++) {
 const itemsArray = document.getElementsByClassName("item");
 let activeItemIndex = 0;
 itemsArray[activeItemIndex].classList.add("active");
+
+//Logica nextBtn
+const prevBtn = document.querySelector(".prev");
+const nextBtn = document.querySelector(".next");
+
+nextBtn.addEventListener("click", function() {
+    prevBtn.classList.remove("hidden")
+
+    if (activeItemIndex < (itemsArray.length - 1)) {
+        itemsArray[activeItemIndex].classList.remove("active");
+        
+        activeItemIndex++;
+
+        itemsArray[activeItemIndex].classList.add("active");
+
+        if (activeItemIndex === itemsArray.length - 1) {
+            nextBtn.classList.add("hidden");
+        }
+    }
+});
+
+//Logica prevBtn
+prevBtn.classList.add("hidden");
+
+prevBtn.addEventListener("click", function () {
+   nextBtn.classList.remove("hidden");
+
+    if (activeItemIndex > 0){
+        itemsArray[activeItemIndex].classList.remove("active");
+
+        activeItemIndex--;
+
+        itemsArray[activeItemIndex].classList.add("active");
+        if (activeItemIndex === 0) {
+            prevBtn.classList.add("hidden")
+        }
+    }
+});
